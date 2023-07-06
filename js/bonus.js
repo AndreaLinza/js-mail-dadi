7a3bfc6aa2ffd75f44825c009a58f2de2e047de2
AndreaLinza <andrealinza@gmail.com>
2023-07-06 17:03:56 +0200

    bonus complete

--- js/bonus.js
+++ js/bonus.js
@@ -1,26 +1,51 @@
-let inputNum = parseInt(document.querySelector(".first-number"));
-//const InputSecondNum = parseInt(document.querySelector(".second-number"));
-//const inputThirdNum = parseInt(document.querySelector(".third-number"));
+const inputFirstNum = document.getElementById("first-number");
+const inputSecondNum = document.getElementById("second-number");
+const inputThirdNum = document.getElementById("third-number");
 const btnThrow = document.querySelector(".btn-throw");
-let COM = document.getElementById("dado-COM"); 
-//let COM2 = document.getElementById("dado-COM"); 
-//let COM3 = document.getElementById("dado-COM"); 
 
 
-const totalNum = inputNum + inputNum+ inputNum ;
-
-btnThrow.addEventListener("click", function(){
+btnThrow.addEventListener("click", function () {
 
 
-    for (let i = 0; i <= 3; i++){
-        
-        COM = Math.floor(Math.random() * 6 + 1);
-        document.getElementById("dado-COM").innerHTML = `${COM}`; 
+
+    const totNum = parseInt(inputFirstNum.value) + parseInt(inputSecondNum.value) + parseInt(inputThirdNum.value);
+    console.log(totNum);
+
+    if (totNum > 18 ||  inputFirstNum.value >= 7 || inputSecondNum.value >= 7 || inputThirdNum.value >= 7 ) {
+        document.getElementById("result").innerHTML = "NON BARARE 😒";
+
     }
-    //COM2 = Math.floor(Math.random() * 6 + 1);
-    //document.getElementById("dado-COM").innerHTML = `${COM2}`; 
-    //COM3 = Math.floor(Math.random() * 6 + 1);
-    //document.getElementById("dado-COM").innerHTML = `${COM3}`;
-    
+    else if(inputFirstNum.value === "" || inputSecondNum.value === "" || inputThirdNum.value === ""){
+        document.getElementById("result").innerHTML = "INSERISCI QAULCOSA DI VALIDO 😒";
+
+    }
+    else {
+
+
+        const COM1 = Math.floor(Math.random() * 6 + 1);
+        document.getElementById("dado-COM1").innerHTML = `${COM1}`;
+        const COM2 = Math.floor(Math.random() * 6 + 1);
+        document.getElementById("dado-COM2").innerHTML = `${COM2}`;
+        const COM3 = Math.floor(Math.random() * 6 + 1);
+        document.getElementById("dado-COM3").innerHTML = `${COM3}`;
+
+        const totCOM = parseInt(COM1) + parseInt(COM2) + parseInt(COM3);
+        console.log(totCOM)
+
+        if (totCOM > totNum) {
+            document.getElementById("result").innerHTML = "HAI PERSO";
+
+        }
+        else if (totCOM === totNum) {
+            document.getElementById("result").innerHTML = "HAI PAREGGIATO";
+
+
+        }
+        else {
+            document.getElementById("result").innerHTML = "HAI VINTO";
+
+        }
+
+    }
 
 })
